7873c7655d86d31d3dacc5357cde9192fdb9d677
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-08-07 11:16:09 +0300

    Fix:Implement all spicy_foods functions and pass full test suite

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,39 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return[food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return[food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat = "🌶" * food["heat_level"]
+        print(f"{name} ({cuisine}) | Heat Level: {heat}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            name = food["name"]
+            cuisine = food["cuisine"]
+            heat = "🌶" * food["heat_level"]
+            print(f"{name} ({cuisine}) | Heat Level: {heat}")
+
     pass
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total = sum(food["heat_level"] for food in spicy_foods)
+    count = len(spicy_foods)
+    return total // count
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
     pass
